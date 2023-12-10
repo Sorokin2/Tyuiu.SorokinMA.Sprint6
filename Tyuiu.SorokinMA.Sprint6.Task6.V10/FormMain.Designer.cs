bebb7ed339edf1f87029296063fbcf88e359458a
Tyuiu.SorokinMA@outlook.com <lorik.sorokin01@mail.ru>
2023-12-10 20:59:12 +0500

    Small FIx

--- Tyuiu.SorokinMA.Sprint6.Task6.V10/FormMain.Designer.cs
+++ Tyuiu.SorokinMA.Sprint6.Task6.V10/FormMain.Designer.cs
@@ -32,27 +32,27 @@ namespace Tyuiu.SorokinMA.Sprint6.Task6.V10
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.panelButtons_SMA = new System.Windows.Forms.Panel();
-            this.panelTask_SMA = new System.Windows.Forms.Panel();
-            this.panelInPut_SMA = new System.Windows.Forms.Panel();
-            this.panelOutPut_SMA = new System.Windows.Forms.Panel();
-            this.splitterInPutAndOutPutPanels_SMA = new System.Windows.Forms.Splitter();
-            this.buttonOpenFile_SMA = new System.Windows.Forms.Button();
-            this.buttonDone_SMA = new System.Windows.Forms.Button();
             this.buttonHelp_SMA = new System.Windows.Forms.Button();
+            this.buttonDone_SMA = new System.Windows.Forms.Button();
+            this.buttonOpenFile_SMA = new System.Windows.Forms.Button();
+            this.panelTask_SMA = new System.Windows.Forms.Panel();
             this.groupBoxTask_SMA = new System.Windows.Forms.GroupBox();
-            this.groupBoxInPut_SMA = new System.Windows.Forms.GroupBox();
-            this.groupBoxOutPut_SMA = new System.Windows.Forms.GroupBox();
-            this.textBoxInPut_SMA = new System.Windows.Forms.TextBox();
-            this.textBoxResult_SMA = new System.Windows.Forms.TextBox();
             this.textBoxTask_SMA = new System.Windows.Forms.TextBox();
+            this.panelInPut_SMA = new System.Windows.Forms.Panel();
+            this.groupBoxInPut_SMA = new System.Windows.Forms.GroupBox();
+            this.textBoxInPut_SMA = new System.Windows.Forms.TextBox();
+            this.panelOutPut_SMA = new System.Windows.Forms.Panel();
+            this.groupBoxOutPut_SMA = new System.Windows.Forms.GroupBox();
+            this.textBoxResult_SMA = new System.Windows.Forms.TextBox();
+            this.splitterInPutAndOutPutPanels_SMA = new System.Windows.Forms.Splitter();
             this.openFileDialogTask_SMA = new System.Windows.Forms.OpenFileDialog();
             this.toolTip_SMA = new System.Windows.Forms.ToolTip(this.components);
             this.panelButtons_SMA.SuspendLayout();
             this.panelTask_SMA.SuspendLayout();
-            this.panelInPut_SMA.SuspendLayout();
-            this.panelOutPut_SMA.SuspendLayout();
             this.groupBoxTask_SMA.SuspendLayout();
+            this.panelInPut_SMA.SuspendLayout();
             this.groupBoxInPut_SMA.SuspendLayout();
+            this.panelOutPut_SMA.SuspendLayout();
             this.groupBoxOutPut_SMA.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -67,63 +67,6 @@ namespace Tyuiu.SorokinMA.Sprint6.Task6.V10
             this.panelButtons_SMA.Size = new System.Drawing.Size(784, 82);
             this.panelButtons_SMA.TabIndex = 0;
             // 
-            // panelTask_SMA
-            // 
-            this.panelTask_SMA.Controls.Add(this.groupBoxTask_SMA);
-            this.panelTask_SMA.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTask_SMA.Location = new System.Drawing.Point(0, 82);
-            this.panelTask_SMA.Name = "panelTask_SMA";
-            this.panelTask_SMA.Size = new System.Drawing.Size(784, 83);
-            this.panelTask_SMA.TabIndex = 0;
-            // 
-            // panelInPut_SMA
-            // 
-            this.panelInPut_SMA.Controls.Add(this.groupBoxInPut_SMA);
-            this.panelInPut_SMA.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelInPut_SMA.Location = new System.Drawing.Point(0, 165);
-            this.panelInPut_SMA.Name = "panelInPut_SMA";
-            this.panelInPut_SMA.Size = new System.Drawing.Size(474, 296);
-            this.panelInPut_SMA.TabIndex = 0;
-            // 
-            // panelOutPut_SMA
-            // 
-            this.panelOutPut_SMA.Controls.Add(this.groupBoxOutPut_SMA);
-            this.panelOutPut_SMA.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelOutPut_SMA.Location = new System.Drawing.Point(474, 165);
-            this.panelOutPut_SMA.Name = "panelOutPut_SMA";
-            this.panelOutPut_SMA.Size = new System.Drawing.Size(310, 296);
-            this.panelOutPut_SMA.TabIndex = 0;
-            // 
-            // splitterInPutAndOutPutPanels_SMA
-            // 
-            this.splitterInPutAndOutPutPanels_SMA.Location = new System.Drawing.Point(474, 165);
-            this.splitterInPutAndOutPutPanels_SMA.Name = "splitterInPutAndOutPutPanels_SMA";
-            this.splitterInPutAndOutPutPanels_SMA.Size = new System.Drawing.Size(3, 296);
-            this.splitterInPutAndOutPutPanels_SMA.TabIndex = 1;
-            this.splitterInPutAndOutPutPanels_SMA.TabStop = false;
-            // 
-            // buttonOpenFile_SMA
-            // 
-            this.buttonOpenFile_SMA.Image = ((System.Drawing.Image)(resources.GetObject("buttonOpenFile_SMA.Image")));
-            this.buttonOpenFile_SMA.Location = new System.Drawing.Point(7, 5);
-            this.buttonOpenFile_SMA.Name = "buttonOpenFile_SMA";
-            this.buttonOpenFile_SMA.Size = new System.Drawing.Size(86, 67);
-            this.buttonOpenFile_SMA.TabIndex = 0;
-            this.toolTip_SMA.SetToolTip(this.buttonOpenFile_SMA, "Открыть файл\r\nВыберите нужный файл для обработки");
-            this.buttonOpenFile_SMA.UseVisualStyleBackColor = true;
-            this.buttonOpenFile_SMA.Click += new System.EventHandler(this.buttonOpenFile_SMA_Click);
-            // 
-            // buttonDone_SMA
-            // 
-            this.buttonDone_SMA.Image = ((System.Drawing.Image)(resources.GetObject("buttonDone_SMA.Image")));
-            this.buttonDone_SMA.Location = new System.Drawing.Point(99, 5);
-            this.buttonDone_SMA.Name = "buttonDone_SMA";
-            this.buttonDone_SMA.Size = new System.Drawing.Size(86, 67);
-            this.buttonDone_SMA.TabIndex = 0;
-            this.toolTip_SMA.SetToolTip(this.buttonDone_SMA, "Производит поиск слов с \"w\" и выводит их");
-            this.buttonDone_SMA.UseVisualStyleBackColor = true;
-            this.buttonDone_SMA.Click += new System.EventHandler(this.buttonDone_SMA_Click);
-            // 
             // buttonHelp_SMA
             // 
             this.buttonHelp_SMA.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
@@ -136,6 +79,38 @@ namespace Tyuiu.SorokinMA.Sprint6.Task6.V10
             this.buttonHelp_SMA.UseVisualStyleBackColor = true;
             this.buttonHelp_SMA.Click += new System.EventHandler(this.buttonHelp_SMA_Click);
             // 
+            // buttonDone_SMA
+            // 
+            this.buttonDone_SMA.Enabled = false;
+            this.buttonDone_SMA.Image = ((System.Drawing.Image)(resources.GetObject("buttonDone_SMA.Image")));
+            this.buttonDone_SMA.Location = new System.Drawing.Point(99, 5);
+            this.buttonDone_SMA.Name = "buttonDone_SMA";
+            this.buttonDone_SMA.Size = new System.Drawing.Size(86, 67);
+            this.buttonDone_SMA.TabIndex = 0;
+            this.toolTip_SMA.SetToolTip(this.buttonDone_SMA, "Производит поиск слов с \"w\" и выводит их");
+            this.buttonDone_SMA.UseVisualStyleBackColor = true;
+            this.buttonDone_SMA.Click += new System.EventHandler(this.buttonDone_SMA_Click);
+            // 
+            // buttonOpenFile_SMA
+            // 
+            this.buttonOpenFile_SMA.Image = ((System.Drawing.Image)(resources.GetObject("buttonOpenFile_SMA.Image")));
+            this.buttonOpenFile_SMA.Location = new System.Drawing.Point(7, 5);
+            this.buttonOpenFile_SMA.Name = "buttonOpenFile_SMA";
+            this.buttonOpenFile_SMA.Size = new System.Drawing.Size(86, 67);
+            this.buttonOpenFile_SMA.TabIndex = 0;
+            this.toolTip_SMA.SetToolTip(this.buttonOpenFile_SMA, "Открыть файл\r\nВыберите нужный файл для обработки");
+            this.buttonOpenFile_SMA.UseVisualStyleBackColor = true;
+            this.buttonOpenFile_SMA.Click += new System.EventHandler(this.buttonOpenFile_SMA_Click);
+            // 
+            // panelTask_SMA
+            // 
+            this.panelTask_SMA.Controls.Add(this.groupBoxTask_SMA);
+            this.panelTask_SMA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTask_SMA.Location = new System.Drawing.Point(0, 82);
+            this.panelTask_SMA.Name = "panelTask_SMA";
+            this.panelTask_SMA.Size = new System.Drawing.Size(784, 83);
+            this.panelTask_SMA.TabIndex = 0;
+            // 
             // groupBoxTask_SMA
             // 
             this.groupBoxTask_SMA.Controls.Add(this.textBoxTask_SMA);
@@ -147,6 +122,27 @@ namespace Tyuiu.SorokinMA.Sprint6.Task6.V10
             this.groupBoxTask_SMA.TabStop = false;
             this.groupBoxTask_SMA.Text = "Условие";
             // 
+            // textBoxTask_SMA
+            // 
+            this.textBoxTask_SMA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxTask_SMA.Location = new System.Drawing.Point(3, 16);
+            this.textBoxTask_SMA.Multiline = true;
+            this.textBoxTask_SMA.Name = "textBoxTask_SMA";
+            this.textBoxTask_SMA.ReadOnly = true;
+            this.textBoxTask_SMA.Size = new System.Drawing.Size(778, 64);
+            this.textBoxTask_SMA.TabIndex = 0;
+            this.textBoxTask_SMA.TabStop = false;
+            this.textBoxTask_SMA.Text = resources.GetString("textBoxTask_SMA.Text");
+            // 
+            // panelInPut_SMA
+            // 
+            this.panelInPut_SMA.Controls.Add(this.groupBoxInPut_SMA);
+            this.panelInPut_SMA.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelInPut_SMA.Location = new System.Drawing.Point(0, 165);
+            this.panelInPut_SMA.Name = "panelInPut_SMA";
+            this.panelInPut_SMA.Size = new System.Drawing.Size(474, 296);
+            this.panelInPut_SMA.TabIndex = 0;
+            // 
             // groupBoxInPut_SMA
             // 
             this.groupBoxInPut_SMA.Controls.Add(this.textBoxInPut_SMA);
@@ -157,17 +153,6 @@ namespace Tyuiu.SorokinMA.Sprint6.Task6.V10
             this.groupBoxInPut_SMA.TabIndex = 0;
             this.groupBoxInPut_SMA.TabStop = false;
             this.groupBoxInPut_SMA.Text = "Ввод:";
-            // 
-            // groupBoxOutPut_SMA
-            // 
-            this.groupBoxOutPut_SMA.Controls.Add(this.textBoxResult_SMA);
-            this.groupBoxOutPut_SMA.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBoxOutPut_SMA.Location = new System.Drawing.Point(0, 0);
-            this.groupBoxOutPut_SMA.Name = "groupBoxOutPut_SMA";
-            this.groupBoxOutPut_SMA.Size = new System.Drawing.Size(310, 296);
-            this.groupBoxOutPut_SMA.TabIndex = 0;
-            this.groupBoxOutPut_SMA.TabStop = false;
-            this.groupBoxOutPut_SMA.Text = "Вывод:";
             // 
             // textBoxInPut_SMA
             // 
@@ -182,6 +167,26 @@ namespace Tyuiu.SorokinMA.Sprint6.Task6.V10
             this.textBoxInPut_SMA.TabIndex = 0;
             this.textBoxInPut_SMA.TabStop = false;
             // 
+            // panelOutPut_SMA
+            // 
+            this.panelOutPut_SMA.Controls.Add(this.groupBoxOutPut_SMA);
+            this.panelOutPut_SMA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelOutPut_SMA.Location = new System.Drawing.Point(474, 165);
+            this.panelOutPut_SMA.Name = "panelOutPut_SMA";
+            this.panelOutPut_SMA.Size = new System.Drawing.Size(310, 296);
+            this.panelOutPut_SMA.TabIndex = 0;
+            // 
+            // groupBoxOutPut_SMA
+            // 
+            this.groupBoxOutPut_SMA.Controls.Add(this.textBoxResult_SMA);
+            this.groupBoxOutPut_SMA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxOutPut_SMA.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxOutPut_SMA.Name = "groupBoxOutPut_SMA";
+            this.groupBoxOutPut_SMA.Size = new System.Drawing.Size(310, 296);
+            this.groupBoxOutPut_SMA.TabIndex = 0;
+            this.groupBoxOutPut_SMA.TabStop = false;
+            this.groupBoxOutPut_SMA.Text = "Вывод:";
+            // 
             // textBoxResult_SMA
             // 
             this.textBoxResult_SMA.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -195,17 +200,13 @@ namespace Tyuiu.SorokinMA.Sprint6.Task6.V10
             this.textBoxResult_SMA.TabIndex = 0;
             this.textBoxResult_SMA.TabStop = false;
             // 
-            // textBoxTask_SMA
+            // splitterInPutAndOutPutPanels_SMA
             // 
-            this.textBoxTask_SMA.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBoxTask_SMA.Location = new System.Drawing.Point(3, 16);
-            this.textBoxTask_SMA.Multiline = true;
-            this.textBoxTask_SMA.Name = "textBoxTask_SMA";
-            this.textBoxTask_SMA.ReadOnly = true;
-            this.textBoxTask_SMA.Size = new System.Drawing.Size(778, 64);
-            this.textBoxTask_SMA.TabIndex = 0;
-            this.textBoxTask_SMA.TabStop = false;
-            this.textBoxTask_SMA.Text = resources.GetString("textBoxTask_SMA.Text");
+            this.splitterInPutAndOutPutPanels_SMA.Location = new System.Drawing.Point(474, 165);
+            this.splitterInPutAndOutPutPanels_SMA.Name = "splitterInPutAndOutPutPanels_SMA";
+            this.splitterInPutAndOutPutPanels_SMA.Size = new System.Drawing.Size(3, 296);
+            this.splitterInPutAndOutPutPanels_SMA.TabIndex = 1;
+            this.splitterInPutAndOutPutPanels_SMA.TabStop = false;
             // 
             // openFileDialogTask_SMA
             // 
@@ -232,12 +233,12 @@ namespace Tyuiu.SorokinMA.Sprint6.Task6.V10
             this.Text = "Спринт 6 | Таск 6 | Вариант 10 | Сорокин М.А.";
             this.panelButtons_SMA.ResumeLayout(false);
             this.panelTask_SMA.ResumeLayout(false);
-            this.panelInPut_SMA.ResumeLayout(false);
-            this.panelOutPut_SMA.ResumeLayout(false);
             this.groupBoxTask_SMA.ResumeLayout(false);
             this.groupBoxTask_SMA.PerformLayout();
+            this.panelInPut_SMA.ResumeLayout(false);
             this.groupBoxInPut_SMA.ResumeLayout(false);
             this.groupBoxInPut_SMA.PerformLayout();
+            this.panelOutPut_SMA.ResumeLayout(false);
             this.groupBoxOutPut_SMA.ResumeLayout(false);
             this.groupBoxOutPut_SMA.PerformLayout();
             this.ResumeLayout(false);
